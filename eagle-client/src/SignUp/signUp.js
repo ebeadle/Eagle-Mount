b1dbd4f8a7448eb93a1c4cdd580126a7adaa0ca8
Erin <e.beadle04@gmail.com>
2017-11-13 16:45:59 -0700

    mobx userStore

--- eagle-client/src/SignUp/signUp.js
+++ eagle-client/src/SignUp/signUp.js
@@ -1,10 +1,66 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
-// var axios = require('axios');
+import {inject, observer} from 'mobx-react';
+var axios = require('axios');
 
-export default class SignUp extends Component {
+var SignUp = observer(class SignUp extends Component {
+constructor(props){
+  super(props)
+  this.state = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    skill: ""
+  }
+  this.handleClick = this.handleClick.bind(this);
+  this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
+  this.handleLastNameChange = this.handleLastNameChange.bind(this);
+  this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  this.handleEmailChange = this.handleEmailChange.bind(this);
+  this.handleSkillChange = this.handleSkillChange.bind(this);
+}
+
+  handleFirstNameChange(event) {
+    this.setState({ firstName: event.target.value});
+  }
+  handleLastNameChange(event) {
+    this.setState({ lastName: event.target.value});
+  }
+  handleEmailChange(event) {
+    this.setState({ email: event.target.value});
+  }
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value});
+  }
+  handleSkillChange(event) {
+    this.setState({ skill: event.target.value});
+  }
+
+  handleClick(){
+    console.log(this.props.userStore);
+    this.props.userStore.signUpUser(
+      {firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      password: this.state.password,
+      skill: this.state.skill
+      }
+    ).then((res)=> {
+      if(res.data.email){
+        console.log("added user");
+        // this.props.history.push('/login');
+      } else {
+        console.log("failed")
+      }
+    }).catch((e)=> {
+      console.log(e)
+    })
+  }
+
   render() {
+    console.log(this.state.firstName);
     
     return (
       <div className='login-form'>
@@ -35,6 +91,8 @@ export default class SignUp extends Component {
                 iconPosition='left'
                 placeholder='First Name'
                 type='text'
+                value={this.state.firstName}
+                onChange={this.handleFirstNameChange}
               />
 
             <Form.Input
@@ -43,12 +101,16 @@ export default class SignUp extends Component {
                 iconPosition='left'
                 placeholder='Last Name'
                 type='text'
+                value={this.state.lastName}
+                onChange={this.handleLastNameChange}
               />
               <Form.Input
                 fluid
                 icon='user'
                 iconPosition='left'
                 placeholder='E-mail address'
+                value={this.state.email}
+                onChange={this.handleEmailChange}
               />
               <Form.Input
                 fluid
@@ -56,6 +118,8 @@ export default class SignUp extends Component {
                 iconPosition='left'
                 placeholder='Password'
                 type='password'
+                value={this.state.password}
+                onChange={this.handlePasswordChange}
               />
 
               <Form.Input
@@ -64,6 +128,9 @@ export default class SignUp extends Component {
                 iconPosition='left'
                 placeholder='Confirm Password'
                 type='password'
+                value={this.state.password}
+                onChange={this.handlePasswordChange}
+
               />
 
                <Form.Input
@@ -72,11 +139,13 @@ export default class SignUp extends Component {
                 iconPosition='left'
                 placeholder='Skill Level'
                 type='text'
+                onChange={this.handleSkillChange}
+                value={this.state.skill}
               />
 
               
   
-              <Button color='blue' fluid size='large'>Sign Up</Button>
+              <Button color='blue' fluid size='large' onClick={this.handleClick} >Sign Up</Button>
             </Segment>
           </Form>
          
@@ -85,4 +154,6 @@ export default class SignUp extends Component {
     </div>
     );
   }
-}
+})
+
+export default withRouter(inject('userStore')(SignUp));
--- eagle-client/src/Stores/UserStore.js
+++ eagle-client/src/Stores/UserStore.js
@@ -1,13 +1,41 @@
-import {extendObservable} from 'mobx';
+import { extendObservable } from 'mobx';
 var axios = require('axios');
 
 
 
 export default class UserStore {
-  constructor(){
+  constructor() {
     extendObservable(this, {
-      user: null,
-      message: null
+      user: {},
+      message: "null",
+      get retrieveUser() {
+        return this.user
+      }
+    })
+    this.signUpUser = this.signUpUser.bind(this);
+    console.log(this.user);
+  }
+
+  signUpUser(newUserObj) {
+    return new Promise((resolve, reject) => {
+      axios.post("/signup",
+        {
+          firstName: newUserObj.firstName,
+          lastName: newUserObj.lastName,
+          email: newUserObj.email,
+          password: newUserObj.password,
+          skill: newUserObj.skill
+        }
+      ).then((userObj) => {
+        if (userObj.data) {
+          this.user = userObj.data
+        } else {
+          console.log("user add failed");
+          reject(userObj);
+        }
+        console.log(userObj);
+        resolve(userObj);
+      })
     })
   }
 }
