e0ed8f6220095f31169f0542196c37f4f368962a
Emily Kimmel <kimmelem@hotmail.com>
2017-11-15 09:51:05 -0700

    added Logout.js file. Testing emily branch?

---
+++ eagle-client/src/Logout/logout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { Button, Form, Grid, Header, Image, Message, Segment, Container } from 'semantic-ui-react'
+import {inject, observer, componentByNodeRegistery} from 'mobx-react';
+
+import React, { Component } from 'react';
+
+export default class  extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
