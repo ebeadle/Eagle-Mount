5c408a25635fb824332048ab428f40aa6c349ada
KB-Spanish-Castle <kylegrantb@gmail.com>
2017-11-17 11:46:34 -0700

    Changes made

--- eagle-client/src/App/App.js
+++ eagle-client/src/App/App.js
@@ -5,7 +5,7 @@ import {
 } from 'react-router-dom';
 import './App.css';
 import SignUp from '../SignUp/signUp';
-import {Provider} from 'mobx-react';
+import { Provider } from 'mobx-react';
 import Login from '../Login/login';
 import Nav from '../NavBar/navBar';
 import UserStore from '../Stores/UserStore';
@@ -13,40 +13,32 @@ import ShiftStore from '../Stores/ShiftStore';
 import Calendar from '../Calendar/calendar';
 import Admin from '../Admin/admin';
 import Logout from '../Logout/logout';
-<<<<<<< HEAD
 import HomePage from '../Home/Home';
-=======
 import Footer from '../Footer/Footer';
 import FancyCalendar from '../FancyCalendar/fancyCalendar';
 
->>>>>>> 319987a1c9697970321ef3c7c18cd130d9d75a45
 
 export default class App extends React.Component {
   render() {
     return (
-<<<<<<< HEAD
-      <Provider userStore={new UserStore()}>
-        <div className="Home-intro">
-=======
       <Provider userStore={new UserStore()} shiftStore={new ShiftStore()}>
-        <div>
->>>>>>> 319987a1c9697970321ef3c7c18cd130d9d75a45
-        <Router>
-          <div>
-            <Nav />
-              <Route path='/Admin' render={()=> <Admin /> }/>
-              <Route path='/Signup' render={()=> <SignUp />}/> 
-              <Route path='/Login' render={()=> <Login />}/> 
-              <Route path='/Logout' render={()=> <Logout />}/> 
-              <Route path='/Calendar' render={()=> <Calendar />}/> 
-              <Route path='/FancyCalendar' render={()=> <FancyCalendar />}/> 
+        <div className="Home-intro">
+          <Router>
+            <div>
+              <Nav />
+              <Route path='/Admin' render={() => <Admin />} />
+              <Route path='/Signup' render={() => <SignUp />} />
+              <Route path='/Login' render={() => <Login />} />
+              <Route path='/Logout' render={() => <Logout />} />
+              <Route path='/Calendar' render={() => <Calendar />} />
+              <Route path='/FancyCalendar' render={() => <FancyCalendar />} />
               <Footer />
-          </div>
-        </ Router>
-        <HomePage />
+            </div>
+          </ Router>
+          <HomePage />
         </div>
-        
-     </Provider>
+
+      </Provider>
     );
   }
 }
