37872acb89c5135aa4e32671476741ba2c65ec72
KB-Spanish-Castle <kylegrantb@gmail.com>
2017-11-28 13:19:36 -0700

    Added Error 404 Web Page

--- eagle-client/src/App/App.js
+++ eagle-client/src/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
-  Route, Redirect,
+  Route, Redirect, Switch
 } from 'react-router-dom';
 import './App.css';
 import SignUp from '../SignUp/signUp';
@@ -10,11 +10,13 @@ import Login from '../Login/login';
 import Nav from '../NavBar/navBar';
 import UserStore from '../Stores/UserStore';
 import ShiftStore from '../Stores/ShiftStore';
+import Calendar from '../Calendar/calendar';
 import Admin from '../Admin/admin';
 import Logout from '../Logout/logout';
 import Footer from '../Footer/footer';
 import FancyCalendar from '../FancyCalendar/fancyCalendar';
 import HomePage from '../Home/home';
+import NotFound from './NotFound.jsx';
 //import 'react-router';
 
 
@@ -26,21 +28,22 @@ export default class App extends Component {
           <Router>
             <div>
               <Nav />
-              <Route path='/Admin' render={() => <Admin />} />
-              <Route path='/Signup' render={() => <SignUp />} />
-              <Route path='/Login' render={() => <Login />} />
-              <Route path='/Logout' render={() => <Logout />} />
-              <Route path='/FancyCalendar' render={() => <FancyCalendar />} />
-              <Route path='/Home' render={()=> <HomePage />} />
+              <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path='/Admin' render={() => <Admin />} />
+                <Route path='/Signup' render={() => <SignUp />} />
+                <Route path='/Login' render={() => <Login />} />
+                <Route path='/Logout' render={() => <Logout />} />
+                <Route path='/Calendar' render={() => <Calendar />} />
+                <Route path='/FancyCalendar' render={() => <FancyCalendar />} />
+                <Route path='/Home' render={() => <HomePage />} />
+                <Route component={NotFound} />
+              </Switch>
               <Footer />
             </div>
           </ Router>
-
         </div>
       </Provider>
     );
   }
 }
-
-
-
