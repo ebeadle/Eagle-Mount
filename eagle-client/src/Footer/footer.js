d56881bacd63a3674c9e274cea1f1066c57364a9
Erin <e.beadle04@gmail.com>
2017-11-30 14:22:06 -0700

    confirm password on signup

--- eagle-client/src/Footer/footer.js
+++ eagle-client/src/Footer/footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-//import Weather from '../weather/Weather';
+import './footer.css';
+
 import {
   Container,
   Grid,
@@ -19,16 +20,16 @@ export default class Footer extends Component {
           <Container>
             <Grid divided inverted stackable>
             <Grid.Column width={5}>
-                <Image as={Link} to="/" alt='Eagle Mount Bozeman' src='https://static1.squarespace.com/static/55e10aede4b023ac4136bb15/566634a9a976afddc7098140/5673106fc647ad862c4cb8f1/1450381490626/Eaglemount.png' />        
+                <Image as={Link} to="/" alt='slope scheduler' src='./img/logo.png' size='small' />        
                 </Grid.Column>
 
                 <Grid.Column width={6}>
                   <Header inverted as='h7'>
                   </Header>
                   <List link inverted>
-                    <a className="item" target="_blank" rel="noopener noreferrer" href="//eaglemount.org/">Eagle Mount Bozeman Official Website</a> 
-                    <Link className="item" to="/contactinfo">Contact Us?</Link>
-                    <a title="Weather at North Entrance" className="item" target="_blank" rel="noopener noreferrer" href='https://forecast.weather.gov/MapClick.php?zoneid=WYZ001'>Current Weather</a> 
+                  
+                  <a title="Bridger Bowl" className="item" target="_blank" rel="noopener noreferrer" href='http://eaglemount.org/'>Eagle Mount Website</a> 
+                    <a title="Bridger Bowl" className="item" target="_blank" rel="noopener noreferrer" href='https://bridgerbowl.com/weather/snow-report'>Bridger Bowl Snow Report</a> 
                   </List>
                 </Grid.Column>
                 {/* <Grid.Column width={5}> */}
